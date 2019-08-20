7657fab806eb2913119cc2054d7f7e58ecbf962c
emersondev <emersonmelody@hotmail.com>
2019-08-20 17:29:33 -0300

    aula 14

---
+++ JavaOnline/src/ControleFluxoDois.java
@@ -0,0 +1,34 @@
+
+public class ControleFluxoDois {
+
+	public static void main(String[] args) {
+		byte dia = 8;
+		
+		// char, int, byte, short, enum, String (esses são os tipos que podem ir no switch)
+		switch(dia) {
+		case 1:
+			System.out.println("Domingo");
+			break;
+		case 2:
+			System.out.println("Segunda");
+			break;
+		case 3:
+			System.out.println("Terca");
+			break;
+		case 4:
+			System.out.println("quarta");
+			break;
+		case 5:
+			System.out.println("Quinta");
+			break;
+		case 6:
+			System.out.println("Sexta");
+			break;
+		case 7:
+			System.out.println("Sabado");
+			break;
+		default:
+			System.out.println("Coloque um dia da semana valido");
+		}
+	}
+}
--- JavaOnline/src/OlaMundo.java
+++ JavaOnline/src/OlaMundo.java
@@ -2,7 +2,7 @@
 public class OlaMundo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hellow World");
 
 	}
 
